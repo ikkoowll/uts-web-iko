5e45296dc13608c6dc76860a2ede8f3354870e7b
ikowl <1202407029@students.itspku.ac.id>
2026-05-13 13:55:53 +0700

    update warna dan update tulisan dasboard

--- login.php
+++ login.php
@@ -36,7 +36,7 @@ if (isset($_POST['login'])) {
         <form method="POST" action="">
             <input type="text" name="username" placeholder="Username" required>
             <input type="password" name="password" placeholder="Password" required>
-            <button type="submit" name="login">Masuk ke Dasbor</button>
+            <button type="submit" name="login">Masuk ke Dashboard</button>
         </form>
 
         <div class="register-link">
