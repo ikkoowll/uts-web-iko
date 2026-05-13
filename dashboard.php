07892b6267ed78c083d32040c78810d361d43d59
ikowl <1202407029@students.itspku.ac.id>
2026-05-13 11:03:45 +0700

    h

--- dashboard.php
+++ dashboard.php
@@ -22,7 +22,7 @@ $result = mysqli_query($conn, "SELECT * FROM anggota_ukm");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard UKM</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css?v=1">
 </head>
 <body>
 
