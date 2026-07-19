061e2a1601868296643dd94960b604f1591e8dcb
ikowl <1202407029@students.itspku.ac.id>
2026-07-19 19:37:27 +0700

    masih upgrade verifikasi

--- config.php
+++ config.php
@@ -16,9 +16,9 @@ if (!$conn) {
 // Silakan sesuaikan nilai di bawah ini dengan provider SMTP Anda (misal Gmail, Mailtrap, Brevo, dll)
 define('SMTP_HOST', 'smtp.gmail.com');       // Host SMTP (misal: smtp.gmail.com atau sandbox.smtp.mailtrap.io)
 define('SMTP_PORT', 587);                    // Port SMTP (587 untuk TLS/STARTTLS, 465 untuk SSL, atau 2525 untuk Mailtrap)
-define('SMTP_USER', 'your-email@gmail.com'); // Username/Email SMTP Anda
-define('SMTP_PASS', 'your-app-password');    // Password SMTP atau App Password (jika menggunakan Gmail)
-define('SMTP_FROM', 'your-email@gmail.com'); // Email pengirim yang akan muncul di inbox penerima
+define('SMTP_USER', 'ikoanggarita@gmail.com'); // Username/Email SMTP Anda
+define('SMTP_PASS', 'cynfzgdtbnbtohdr');    // MASUKKAN 16-KARAKTER SANDI APLIKASI GOOGLE ANDA DI SINI
+define('SMTP_FROM', 'ikoanggarita@gmail.com'); // Email pengirim yang akan muncul di inbox penerima
 define('SMTP_FROM_NAME', 'SIM HIMATIF');     // Nama pengirim yang muncul
 define('SMTP_SECURE', 'tls');                // Pengaman (tls atau ssl)
 ?>
